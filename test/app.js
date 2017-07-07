44423f3231481dbca8c86caf5904819d3ac9ef3e
sophiewigmore <sophiemala@yahoo.com>
2017-07-07 11:15:34 -0400

    changes

--- test/app.js
+++ test/app.js
@@ -10,10 +10,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds153521.mlab.com:53521/heroku_mll2fxdw'; // user yours
 }
-// Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
-// above with your own URL given to you by mLab
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
