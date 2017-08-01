2fc2cc71328eda352e718244a0116b12b614162f
sophiewigmore <sophiemala@yahoo.com>
2017-08-01 18:03:41 -0400

    got rid of local arrays

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -7,12 +7,6 @@
         .factory("userService", userService);
 
     function userService($http) {
-        var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", isAdmin: true},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
-        ];
         var api = {
             "findUserByCredentials": findUserByCredentials,
             "findUserById": findUserById,
